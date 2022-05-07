8eff8c29c48eccd3eb459dbd9bb1a0ba78385a09
popovanton0 <popovanton0+github@gmail.com>
2022-05-07 15:53:59 +0300

    Dark theme support

--- kira/src/main/java/com/popovanton0/kira/Kira.kt
+++ kira/src/main/java/com/popovanton0/kira/Kira.kt
@@ -1,15 +1,14 @@
 package com.popovanton0.kira
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -28,34 +27,34 @@ public fun TextCard(
     cornerRadius: Dp = 12.dp
 ) {
     val shape = RoundedCornerShape(cornerRadius)
-    Column(
-        modifier = Modifier
-            .shadow(8.dp, shape)
-            .background(Color.White, shape)
-            .padding(12.dp)
-            .animateContentSize(),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        Text(
-            text = text,
-            color = if (isRed) Color.Red else Color.Unspecified
-        )
-        Text(
-            text = "skill is " + when (skill) {
-                Skill.LOW -> "😤"
-                Skill.OK -> "🙂"
-                Skill.SICK -> "😎"
-                null -> "missing"
-            },
-            fontSize = 18.sp,
-        )
-        Text(
-            text = "🍔 is " + when (food) {
-                Food.BAD -> "😤"
-                Food.GOOD -> "🙂"
-                Food.EXCELLENT -> "😃"
-            },
-            fontSize = 18.sp,
-        )
+    Card(elevation = 8.dp, shape = shape) {
+        Column(
+            modifier = Modifier
+                .animateContentSize()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Text(
+                text = text,
+                color = if (isRed) Color.Red else Color.Unspecified
+            )
+            Text(
+                text = "skill is " + when (skill) {
+                    Skill.LOW -> "😤"
+                    Skill.OK -> "🙂"
+                    Skill.SICK -> "😎"
+                    null -> "missing"
+                },
+                fontSize = 18.sp,
+            )
+            Text(
+                text = "🍔 is " + when (food) {
+                    Food.BAD -> "😤"
+                    Food.GOOD -> "🙂"
+                    Food.EXCELLENT -> "😃"
+                },
+                fontSize = 18.sp,
+            )
+        }
     }
 }
