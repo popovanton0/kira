182fe65ebac51d4e70a3e046b2d6c763a022dd40
popovanton0 <popovanton0+github@gmail.com>
2022-06-10 20:45:26 +0300

    Integration of Type API into Dropdown UI

--- kira/src/main/java/com/popovanton0/kira/ui/Dropdown.kt
+++ kira/src/main/java/com/popovanton0/kira/ui/Dropdown.kt
@@ -1,12 +1,8 @@
-@file:OptIn(ExperimentalMaterialApi::class)
-
 package com.popovanton0.kira.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +10,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.popovanton0.kira.suppliers.base.Type
 
 @Composable
 internal fun Dropdown(
@@ -21,13 +18,14 @@ internal fun Dropdown(
     onSelect: (index: Int) -> Unit,
     options: List<String>,
     label: String,
+    type: Type,
 ) {
     var expanded by remember { mutableStateOf(false) }
     ListItem(
-        modifier = Modifier.clickable { expanded = true },
+        modifier = Modifier.clickable { expanded = options.size > 1 },
+        overlineText = { TypeUi(type) },
         text = { Text(text = label) },
-        trailing = {
-            Text(text = options[selectedOptionIndex])
+        end = {
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false }
@@ -38,6 +36,7 @@ internal fun Dropdown(
                     }
                 }
             }
+            Text(text = options[selectedOptionIndex])
         }
     )
 }
