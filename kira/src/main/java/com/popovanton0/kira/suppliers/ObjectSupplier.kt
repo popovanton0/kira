16078b0b17b968611203724799d6492789b756c8
popovanton0 <popovanton0+github@gmail.com>
2022-06-11 15:10:12 +0300

    ObjectSupplierBuilder created

--- kira/src/main/java/com/popovanton0/kira/suppliers/ObjectSupplier.kt
+++ kira/src/main/java/com/popovanton0/kira/suppliers/ObjectSupplier.kt
@@ -4,13 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.popovanton0.kira.suppliers.base.ClassType
 import com.popovanton0.kira.suppliers.base.ReflectionUsage
+import com.popovanton0.kira.suppliers.base.Supplier
+import com.popovanton0.kira.suppliers.base.SupplierBuilder
 import com.popovanton0.kira.suppliers.base.Ui
 import com.popovanton0.kira.suppliers.compound.KiraScope
 
 @ReflectionUsage
 public inline fun <reified T : Any> KiraScope.`object`(
     paramName: String,
-): OneOfManySupplierBuilder<T> {
+): ObjectSupplierBuilder<T> {
     val kClass = T::class
     val qualifiedName = kClass.qualifiedName!!
     val value = kClass.objectInstance ?: notAnObjectError(qualifiedName)
@@ -21,7 +23,7 @@ public inline fun <reified T : Any> KiraScope.`object`(
 public inline fun <reified T : Any> KiraScope.nullableObject(
     paramName: String,
     isNullByDefault: Boolean = true,
-): OneOfManySupplierBuilder<T?> {
+): NullableObjectSupplierBuilder<T> {
     val kClass = T::class
     val qualifiedName = kClass.qualifiedName!!
     val value = kClass.objectInstance ?: notAnObjectError(qualifiedName)
@@ -36,21 +38,44 @@ public fun <T : Any> KiraScope.`object`(
     paramName: String,
     qualifiedName: String,
     value: T,
-): OneOfManySupplierBuilder<T> {
-    val type = ClassType(qualifiedName, ClassType.Variant.OBJECT)
-    val objectValueName = qualifiedName.substringAfterLast('.')
-    return singleValue(paramName, value withName objectValueName, type)
-}
+): ObjectSupplierBuilder<T> =
+    ObjectSupplierBuilder(paramName, qualifiedName, value).also(::addSupplier)
 
 public fun <T : Any> KiraScope.nullableObject(
     paramName: String,
     qualifiedName: String,
     value: T,
     isNullByDefault: Boolean = true,
-): OneOfManySupplierBuilder<T?> {
-    val type = ClassType(qualifiedName, ClassType.Variant.OBJECT)
-    val objectValueName = qualifiedName.substringAfterLast('.')
-    return nullableSingleValue(paramName, value withName objectValueName, type, isNullByDefault)
+): NullableObjectSupplierBuilder<T> =
+    NullableObjectSupplierBuilder(paramName, qualifiedName, value, isNullByDefault)
+        .also(::addSupplier)
+
+public class ObjectSupplierBuilder<T : Any> internal constructor(
+    public var paramName: String,
+    public var qualifiedName: String,
+    public val value: T,
+) : SupplierBuilder<T>() {
+    override fun BuildKey.build(): Supplier<T> {
+        val type = ClassType(qualifiedName, ClassType.Variant.OBJECT)
+        val objectValueName = qualifiedName.substringAfterLast('.')
+        return KiraScope().singleValue(paramName, value withName objectValueName, type)
+            .apply { initialize() }
+    }
+}
+
+public class NullableObjectSupplierBuilder<T : Any> internal constructor(
+    public var paramName: String,
+    public var qualifiedName: String,
+    public val value: T,
+    public var isNullByDefault: Boolean,
+) : SupplierBuilder<T?>() {
+    override fun BuildKey.build(): Supplier<T?> {
+        val type = ClassType(qualifiedName, ClassType.Variant.OBJECT)
+        val objectValueName = qualifiedName.substringAfterLast('.')
+        return KiraScope().nullableSingleValue(
+            paramName, value withName objectValueName, type, isNullByDefault
+        ).apply { initialize() }
+    }
 }
 
 private object Asd
