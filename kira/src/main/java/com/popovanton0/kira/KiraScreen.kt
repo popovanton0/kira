dfb1a39fe96e306f35c276423e382c0b473995ac
popovanton0 <popovanton0+github@gmail.com>
2022-06-11 23:18:19 +0300

    Kira supplier first build moved to a background thread

--- kira/src/main/java/com/popovanton0/kira/KiraScreen.kt
+++ kira/src/main/java/com/popovanton0/kira/KiraScreen.kt
@@ -1,24 +1,32 @@
 package com.popovanton0.kira
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.popovanton0.kira.suppliers.Kira
 import com.popovanton0.kira.suppliers.KiraProvider
+import com.popovanton0.kira.suppliers.base.ReflectionUsage
+import com.popovanton0.kira.suppliers.base.Supplier
 import com.popovanton0.kira.suppliers.base.Ui
 import com.popovanton0.kira.suppliers.compound.KiraScope
 import com.popovanton0.kira.suppliers.compound.injector
 import com.popovanton0.kira.ui.DefaultHeader
 import com.popovanton0.kira.ui.EarlyPreview
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-private class KiraViewModel(val kiraProvider: KiraProvider<*>) : ViewModel() {
-    init {
-        kiraProvider.kira.build()
-    }
-}
+private class KiraViewModel(val kiraProvider: KiraProvider<*>) : ViewModel()
 
 @Composable
 public fun <Scope : KiraScope> KiraScreen(
@@ -45,8 +53,17 @@ public fun KiraScreen(
     val factory = remember { KiraViewModelFactory(kiraProvider, header) }
     val vm = viewModel<KiraViewModel>(factory = factory)
 
-    Box {
-        vm.kiraProvider.kira.build().Ui()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        var supplier by remember { mutableStateOf<Supplier<Unit>?>(null) }
+            /** building can be slow, especially if [ReflectionUsage] APIs were used */
+            /** thus building is performed on the background thread */
+        LaunchedEffect(true) {
+            withContext(Dispatchers.IO) { supplier = vm.kiraProvider.kira.build() }
+        }
+        if (supplier == null) CircularProgressIndicator() else supplier!!.Ui()
         EarlyPreview()
     }
 }
